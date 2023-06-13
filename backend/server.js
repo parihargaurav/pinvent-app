13daf16e26cc33706aab6632ff44099c5dc6ae00
Gaurav <parihargaurav527@gmail.com>
2023-06-13 15:23:38 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,34 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const userRoute = require("./routes/userRoute");
+const errorHandler = require("./middleWare/errorMiddleware");
+const cookieParser = require("cookie-parser");
+
 
 const app = express()
 
-const PORT = process.env.PORT || 5000;
+// Middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Routes Middleware
+app.use("/api/users", userRoute);
+
+// Routes
+app.get("/", (req, res) => {
+    res.send("Home Page");
+});
+
+// Error Middleware
+app.use(errorHandler);
+
 
 // Connect to DB and start server
+
+const PORT = process.env.PORT || 5000;
 
 mongoose
     .connect(process.env.MONGO_URI)
